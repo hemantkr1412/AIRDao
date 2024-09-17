fb318197907bef469d957c95db6be3127d278d5e
Hemant <hemantfbzrana@gmail.com>
2024-09-18 00:19:19 +0530

    wallet connection and bugs

---
+++ build/assets/sign-DNcP9RfW.js
@@ -0,0 +1,1 @@
+import{v as n}from"./session-request-C8SH1vJo.js";import"./index-CVGbB_rt.js";async function o(e){const{account:s,params:a}=e;return n(s,a[1]),s.signMessage({message:{raw:a[0]}})}export{o as handleSignRequest};
